5351507e986ee808595b5b795270cb18473b1042
PrakashKumawat2003 <prakash.kumawat@buildinclicks.com>
2024-07-15 07:57:17 +0530

    table

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,8 @@ import Box from "./_component/box";
 import DivergencCalculator from "./_component/DivergenceCalculator";
 import DMXBinaryConverter from "./_component/DMXAddressCalculator";
 import AngleDistanceSizeCalculator from "./_component/AngleDistanceSizeCalculator";
+import ComparisionTable from "./_component/ComparisionTable";
 
 export default function Home() {
-  return <AngleDistanceSizeCalculator />;
+  return <ComparisionTable />;
 }
